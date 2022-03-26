735ad681a426202c91ab7d4192463111df0abfd3
RamziGueddiche21 <ramzi.gueddiche@esprit.tn>
2022-03-26 08:54:13 +0100

    gggg

---
+++ src/test/java/projet1/ProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package projet1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
